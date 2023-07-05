0134bdad9610493b28a0e394e5579c85118fdf28
popplio2 <danikbrisk@yahoo.com>
2023-07-05 00:13:56 -0400

    beta friend requests

--- showsocial/src/views/UserView.vue
+++ showsocial/src/views/UserView.vue
@@ -4,6 +4,10 @@
       <ShowComponent v-for="show in showArray" :show="show.show" :isAddable="true" :key="show.show.id"/>
     </div> -->
     <h1>{{ user.username }}'s Posts</h1>
+
+    <button @click="requestFriend(user.username)" v-if="!isRequested">Send friend request</button>
+    <button @click="unsendRequest(user.username)" v-else>Requested</button>
+
     <div class="posts">
       <ShowPost v-for="post in user.posts" :post="post" :key="post.id"/>
     </div>
@@ -39,6 +43,7 @@ export default {
         }
       ],
       user: null,
+      isRequested: false,
       hasUser: false,
     }
   },
@@ -49,10 +54,38 @@ export default {
   },
   methods: {
     fetchData() {
-      this.user = this.userSample.find(user => user.username === this.$route.params.username)
+      this.user = this.userSample.find(user => user.username === this.$route.params.username);
+      this.checkRequested();
       this.hasUser = true;
     },
-  }
+    requestFriend(username) {
+      const user = this.userSample.find(user => user.username === username); //finds other user
+      const request = {
+        sender: this.userStore.username,
+        receiver: user.username
+      };
+      this.userStore.friendRequests.push(request);
+      user.friendRequests.push(request);
+      this.checkRequested();
+    },
+    unsendRequest(username) {
+      if (confirm("Do you want to unsend your friend request?") == true) {
+        const user = this.userSample.find(user => user.username === username);
+        const request = user.friendRequests.find(request => request.sender === this.userStore.username);
+        user.friendRequests.pop(request); //deletes request from other user
+        const myRequest = this.userStore.friendRequests.find(request => request.receiver === this.user.username);
+        this.userStore.friendRequests.pop(myRequest); //deletes request from your data
+      }
+      this.checkRequested();
+    },
+    checkRequested() {
+      if (this.userStore.friendRequests.find(request => request.receiver === this.user.username)) {
+        this.isRequested = true;
+      } else {
+        this.isRequested = false;
+      }
+    }
+  } 
 }
 </script>
 
